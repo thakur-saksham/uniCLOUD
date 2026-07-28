230f061d71a837bb64126d05f1717fb901d53ae8
thakur-saksham <saksham.27908@stu.upes.ac.in>
2026-07-28 22:50:38 +0530

    UI: Dim orb canvas, make orb dark purple, fix mobile nav overlap

--- js/orb.js
+++ js/orb.js
@@ -107,9 +107,9 @@ export function initOrb(containerId) {
       return vec4(colorIn.rgb / (a + 1e-5), a);
     }
 
-    const vec3 baseColor1 = vec3(0.611765, 0.262745, 0.996078);
-    const vec3 baseColor2 = vec3(0.298039, 0.760784, 0.913725);
-    const vec3 baseColor3 = vec3(0.062745, 0.078431, 0.600000);
+    const vec3 baseColor1 = vec3(0.356, 0.129, 0.713); // Darker purple
+    const vec3 baseColor2 = vec3(0.231, 0.027, 0.392); // Very dark purple
+    const vec3 baseColor3 = vec3(0.090, 0.000, 0.168); // Almost black purple
     const float innerRadius = 0.6;
     const float noiseScale = 0.65;
 
